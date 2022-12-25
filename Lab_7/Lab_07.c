1a9259670d0eac259f1ada8d719820829417a31d
Daniil Raziel <75831226+Raziel536@users.noreply.github.com>
2022-12-25 16:50:01 +0500

    Update Lab_07.c

--- Lab_7/Lab_07.c
+++ Lab_7/Lab_07.c
@@ -1,1 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <pthread.h> 
+#include <fcntl.h>
+#include <stdlib.h>
 
+pthread_rwlock_t rwl;
+int count = 0;
+
+void* writer(void* args) {
+    (void)args;
+    for(int i=0; i < 10; ++i) {
+        pthread_rwlock_wrlock(&rwl);
+	    ++count;
+	    sleep(2);
+	    pthread_rwlock_unlock(&rwl);
+	    sleep(2);
+    }
+    pthread_exit(0);
+}
+
+void* reader(void* args) {
+	(void)args;
+	for(int i=0; i < 10; ++i) {
+		pthread_rwlock_rdlock(&rwl);
+		printf("counter: %d tid: %ld\n", count, pthread_self());
+		sleep(2);
+		pthread_rwlock_unlock(&rwl);
+		sleep(2); 
+	}
+	pthread_exit(0);
+}
+
+int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+	int arrs = 10;
+	pthread_t w_tid;
+	pthread_t r_tid[arrs];
+	pthread_rwlock_init(&rwl, NULL);
+
+	pthread_create(&w_tid, NULL, writer, NULL);
+	for(int i = 0; i < arrs; ++i) {
+		pthread_create(&r_tid[i], NULL, reader, NULL);
+	}
+
+	pthread_join(w_tid, NULL);
+	for(int i = 0; i < arrs; ++i) {
+		pthread_join(r_tid[i], NULL);
+	}
+    pthread_rwlock_destroy(&rwl);
+	return 0;
+}
